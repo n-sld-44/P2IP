5a2e4dcd0ae0914d1da2304cdfea5f14972cd526
Nath <148266212+n-sld-44@users.noreply.github.com>
2024-06-09 12:14:56 +0200

    c

--- Code/main.py
+++ Code/main.py
@@ -1,18 +1,18 @@
+
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun  3 15:10:47 2024
+Created on Sat Apr 20 18:01:06 2024
 
-@author: Julien GILLES
+@author: maria
 """
+
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, PhotoImage
 import shutil
 from PIL import Image, ImageTk
 #from moviepy.editor import VideoFileClip
 import cv2
-import tkinter.ttk as ttk
-
-
+import tkinter.ttk as ttk 
 
 def select_video():
     file_path = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4;*.avi;*.mov")])
@@ -29,30 +29,27 @@ def select_video():
             label_file_path.config(text="Erreur lors de l'enregistrement de la vidéo : " + str(e), fg="red")
             
 
-
-def custom_yesno_dialog(message): 
-    global response
-    response = None  # Initialisation de la réponse
-
+def custom_yesno_dialog(message): #permet d'afficher la fenêtre qui demande à l'utilisateur s'il souhaite visionner la vidéo
+    
+    response = [False]  # Utiliser une liste pour pouvoir la modifier dans les sous-fonctions
+    
+    def on_yes():
+        response[0] = True
+        dialog.destroy()
+    
+    def on_no():
+        response[0] = False
+        dialog.destroy()
+    
     dialog = tk.Toplevel()
     dialog.title("Confirmation")
+    dialog.iconphoto(False,logo)
     
     label = tk.Label(dialog, text=message)
     label.pack(padx=20, pady=10)
 
-    def on_yes():
-        global response
-        response = True
-        dialog.destroy()
-        root.quit()  # Quitter l'application si l'utilisateur choisit "Oui"
-
     yes_button = tk.Button(dialog, text="Oui", command=on_yes)
     yes_button.pack(side="left", padx=10, pady=10)
-
-    def on_no():
-        global response
-        response = False
-        dialog.destroy()
 
     no_button = tk.Button(dialog, text="Non", command=on_no)
     no_button.pack(side="right", padx=10, pady=10)
@@ -60,8 +57,9 @@ def custom_yesno_dialog(message):
     dialog.focus_set()
     dialog.grab_set()
     dialog.wait_window()
+    
+    return response[0]
 
-    return response
 
 def show_video(video_path):
     cap = cv2.VideoCapture(video_path)
@@ -73,41 +71,39 @@ def show_video(video_path):
                 break
         else:
             break
-    
     cap.release()
     cv2.destroyAllWindows()
 
-"""def show_video(video_path):
-    video_clip = VideoFileClip(video_path)
-    video_clip.preview()"""
+      
             
-
-# Définit la taille de la fenêtre
+# Définit la taille de la fenêtre   
 root = tk.Tk()
 root.title("SignTrad")
-root.state('zoomed')
+root.state('zoomed')  
 
-# Création d'un canevas (Canvas) pour le fond d'écran 
+logo = PhotoImage(file='./Files/images_interface/logo.png')
+root.iconphoto(False, logo)
+# Création d'un canevas (Canvas) pour le fond d'écran bleu marine
 canvas = tk.Canvas(root, width=1000, height=600, bg="white")
 canvas.pack(expand=True, fill="both")  # Le canevas s'étend pour remplir toute la fenêtre
 
 # Chargement de l'image et conversion en ImageTk
-image_path = "./Files/images_interface/SignTrad.png"
+image_path = r"./Files/images_interface/Interface.png"
 image = Image.open(image_path)
 image = image.resize((1500, 1100))
 photo = ImageTk.PhotoImage(image)
 
 # Création du label avec l'image et placement sur le canevas
 label_image = tk.Label(canvas, image=photo, bg="white")
-label_image.pack(expand=True)
+label_image.pack(expand = True)
 
 # Création d'une frame pour afficher les messages
 message_frame = tk.Frame(root, bg="white")
 message_frame.place(relx=0.5, rely=0.9, anchor="center", relwidth=0.8, relheight=0.1)
 
 # Label pour afficher le chemin du fichier sélectionné et placement sur le canevas
-label_file_path = tk.Label(message_frame, text="", bg="white", fg="white")
-label_file_path.pack(expand=True)
+label_file_path = tk.Label(message_frame, text="", bg="white", fg="black")
+label_file_path.pack(expand = True)
 
 # Appliquer un style au bouton
 style = ttk.Style()
@@ -122,5 +118,5 @@ style.configure("TButton",
 select_button = ttk.Button(root, text="Démarrer", command=select_video, style="TButton")
 select_button.place(relx=0.75, rely=0.5, anchor='center')
 
-root.mainloop()
 
+root.mainloop()
