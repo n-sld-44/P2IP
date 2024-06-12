0431f8e829e17abe7d0d54ea06cc91b38a088995
Nath <148266212+n-sld-44@users.noreply.github.com>
2024-06-12 10:32:00 +0200

    12/06

--- Code/movie_clip.py
+++ Code/movie_clip.py
@@ -1,4 +1,4 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips, CompositeVideoClip
+from moviepy.editor import VideoFileClip, concatenate_videoclips, CompositeVideoClip, vfx
 
 def word_to_clip(word_list):
     clip_list = list()
@@ -20,15 +20,19 @@ def clip_and_overlay():
     main = VideoFileClip("./Files/video/video.mp4")
     overlay = VideoFileClip("./Files/video/overlay.mp4")
 
+    ratio = overlay.duration/main.duration
+    overlay = overlay.fx(vfx.speedx,ratio)
     #Parametre a changer pour modifier la taille de l'overlay
-    overlay = overlay.resize(height=50)
+    overlay = overlay.resize(height=200)
 
     #Parametre a changer pour modifier l'endroit de l'overlay
-    overlay_position = (50, 50) 
-    """ Pour placer l'overlay en bas à droite
+    overlay_position = (100, 100) 
+    
     overlay_position = (main.size[0] - overlay.size[0],main.size[1] - overlay.size[1])
-    """
+    
     final = CompositeVideoClip([main, overlay.set_pos(overlay_position)])
-    final.write_videofile("video_finale.mp4", codec="libx264")
+    final.write_videofile("./Files/video/video_finale.mp4", codec="libx264")
 
 
+concatenate_sign_clip(['salut','ça_va','oui','super','et_toi','être_fatigué','ah_bon','pourquoi','nuit','récemment','dormir','mal','désolé','que_fais_tu','samedi','prochain','week_end','aimer','se_balader','famille','ensemble','aller','restaurant','amis','ensemble','et_toi', 'adorer','aller','courir','journée','fin','parc','accompagner','accompagner_tu'])
+clip_and_overlay()
